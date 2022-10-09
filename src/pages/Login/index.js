aa57c772ce74fe827562a20f4f3e508aa15e8d5e
zhangpeng12 <zhangpeng12@xiaomi.com>
2022-10-09 17:26:10 +0800

    一些修改

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,7 @@
+// import { Form, Input } from 'antd-mobile'
+import styles from './index.module.less'
 const Login = () => {
-  return <div>login</div>
+  return <div className={styles.Login}>login</div>
 }
 
 export default Login;
