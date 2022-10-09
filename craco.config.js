8fadca01c80406696d5490a001cb848bc28b4dae
zhangpeng12 <zhangpeng12@xiaomi.com>
2022-10-09 18:02:34 +0800

    重置webpack配置

---
+++ craco.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+const CracoLessPlugin = require('craco-less')
+module.exports = {
+  webpack: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+}
